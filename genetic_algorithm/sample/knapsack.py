fe5232eec1fd0d487db90b1e658e4227ed544b20
TakumaKakinoue <kakinouet@gmail.com>
2024-08-23 11:39:33 +0900

    feat:commit knapsack

--- genetic_algorithm/sample/knapsack.py
+++ genetic_algorithm/sample/knapsack.py
@@ -5,41 +5,44 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
 
 import random
 
-from core import GeneticAlgorithm
+from core import MuCommaLambdaGA, MuPlusLambdaGA
 from crossover import one_point
 from mutation import random_int
 from selection import elite
 
+prices = [100, 200, 650, 1100, 2100, 3300]
+values = [2, 5, 16, 32, 64, 105]
+money = 40000
+
 
 def generate_func():
     ans = [0] * 6
-    for _ in range(300):
+    for _ in range(200):
         i = random.randint(0, 5)
         ans[i] += 1
         if objective_func(ans) == 0:
             ans[i] -= 1
             return ans
+    ans[random.randint(0, 5)] = 0
     return ans
 
 
 def objective_func(ans):
-    prices = [100, 200, 300, 650, 1100, 1500]
-    values = [2, 5, 8, 16, 32, 46]
-    money = 30000
     total_value = 0
+    _money = money
     for i, n in enumerate(ans):
-        money -= prices[i] * n
+        _money -= prices[i] * n
         total_value += values[i] * n
-    if money < 0:
+    if _money < 0:
         total_value = 0
     return [total_value]
 
 
-selction_func = elite(30)
+selction_func = elite(100)
 crossover_func = one_point()
-mutation_func = random_int(1, 100, 0.3, 0.4)
+mutation_func = random_int(0, 100, 0.3, 0.4)
 
-ga = GeneticAlgorithm(
+ga = MuPlusLambdaGA(
     "maximize",
     objective_func,
     generate_func,
@@ -47,5 +50,6 @@ ga = GeneticAlgorithm(
     crossover_func,
     mutation_func,
 )
-ans = ga.solve(n_generations=5000, n_population=100)[0]
-print(ans.objective, ans.chromosome)
+ans = ga.solve(n_generations=1000, population_size=200)
+ans.sort(key=lambda x: x.objective, reverse=True)
+print(ans[0].objective, ans[0].chromosome)
